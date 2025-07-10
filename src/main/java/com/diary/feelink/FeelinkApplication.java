85864babdb5006db09f3c3e91a36103af2e2576f
soyun <ehouse190@gmail.com>
2025-07-10 22:04:00 +0900

    init

---
+++ src/main/java/com/diary/feelink/FeelinkApplication.java
@@ -0,0 +1,13 @@
+package com.diary.feelink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeelinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeelinkApplication.class, args);
+    }
+
+}
