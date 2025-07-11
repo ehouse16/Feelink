b5c641c3dbf3072edb4f3e081831e03a16f82d60
soyun <ehouse190@gmail.com>
2025-07-11 12:09:19 +0900

    feat: 회원가입 제약조건, 중복체크 추가

--- src/main/java/com/diary/feelink/member/repository/MemberRepository.java
+++ src/main/java/com/diary/feelink/member/repository/MemberRepository.java
@@ -1,7 +1,12 @@
 package com.diary.feelink.member.repository;
 
 import com.diary.feelink.member.entity.Member;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    boolean existsByEmail(String email);
+    boolean existsByNickname(String nickname);
 }
